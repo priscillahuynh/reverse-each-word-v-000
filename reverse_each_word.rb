8c16f8b9c771e646ba200472f685349527a576fc
Priscilla H <priscillamhuynh@gmail.com>
2018-06-22 10:37:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 def reverse_each_word(sentence)
-  array= sentence.split(" ")
-  array.each do |word|
+  array= sentence.split(" ").collect do |word|
     word.reverse
   end
-  # sentence.each do |word|
-    # word.reverse
-  # end
+  array.join(" ")
 end
